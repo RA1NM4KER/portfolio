323e27cd7ba83c97ec0ba2e46e89ae942c74b750
Kefas Aleck <24862940@sun.ac.za>
2026-03-29 19:36:47 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@xyflow/react/dist/style.css";
 import React from "react";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
           <div className="f1-stripe" />
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
